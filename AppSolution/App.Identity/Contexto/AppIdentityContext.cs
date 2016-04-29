df3aad44a43cfe93d39344bdb3e16b6880fdcda1
allan.freitas <freitasallan@gmail.com>
2016-04-29 16:30:01 -0300

    Pequenas alterações...

--- AppSolution/App.Identity/Contexto/AppIdentityContext.cs
+++ AppSolution/App.Identity/Contexto/AppIdentityContext.cs
@@ -1,17 +1,13 @@
 ﻿using App.Identity.Model;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App.Identity.Contexto
 {
     public class AppIdentityContext : IdentityDbContext<ApplicationUser>, IDisposable
     {
         public AppIdentityContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("AppConnect", throwIfV1Schema: false)
         {
         }
 
--- AppSolution/App.IoC/BootStrapper.cs
+++ AppSolution/App.IoC/BootStrapper.cs
@@ -1,6 +1,7 @@
 ﻿using App.Business.Implementation;
 using App.Business.Interface;
 using App.Identity.Configuration;
+using App.Identity.Contexto;
 using App.Identity.Model;
 using App.Repository.Contexto;
 using App.Repository.Implementation;
@@ -22,7 +23,7 @@ namespace App.IoC
             container.RegisterPerWebRequest<IUsuarioBusiness, UsuarioBusiness>();
 
             //Asp.Net.Identity
-            container.RegisterPerWebRequest<IUserStore<ApplicationUser>>(() => new UserStore<ApplicationUser>(container.GetInstance<AppContext>()));
+            container.RegisterPerWebRequest<IUserStore<ApplicationUser>>(() => new UserStore<ApplicationUser>(new AppIdentityContext()));
             container.RegisterPerWebRequest<IRoleStore<IdentityRole, string>>(() => new RoleStore<IdentityRole>());
             container.RegisterPerWebRequest<ApplicationRoleManager>();
             container.RegisterPerWebRequest<ApplicationUserManager>();
