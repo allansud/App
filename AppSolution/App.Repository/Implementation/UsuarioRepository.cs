f114a759115f07a42a1f9d6d45540591cf15a4e1
allan.freitas <freitasallan@gmail.com>
2016-05-06 18:02:59 -0300

    Começando alterações no Store....

--- AppSolution/App.Repository/Implementation/UsuarioRepository.cs
+++ AppSolution/App.Repository/Implementation/UsuarioRepository.cs
@@ -1,18 +1,73 @@
 ﻿using System;
-using App.Domain.Entities;
 using App.Repository.Contexto;
-using App.Repository.Interface;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
 
 namespace App.Repository.Implementation
 {
-    public class UsuarioRepository : RepositoryBase<Usuario>, IUsuarioRepository
+    public class UsuarioRepository<TEntity, TKey> : IUserStore<TEntity, TKey>, IUserPasswordStore<TEntity, TKey>, IUserSecurityStampStore<TEntity, TKey> 
+        where TEntity : class, IUser<TKey>
     {
-        public UsuarioRepository(AppContext contexto)
-            : base(contexto) { }
+        private AppContext _context = null;
 
-        public void DesativarLock(string id)
+        public UsuarioRepository(AppContext contexto)
         {
-            Set.Find(id).LockoutEnabled = false;
+            _context = contexto;
+        }
+
+        public Task CreateAsync(TEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(TEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> FindByIdAsync(TKey userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> FindByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(TEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetSecurityStampAsync(TEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HasPasswordAsync(TEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(TEntity user, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetSecurityStampAsync(TEntity user, string stamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity user)
+        {
+            throw new NotImplementedException();
         }
     }
 }
